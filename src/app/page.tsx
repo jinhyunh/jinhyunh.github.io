854078e1894d9e031b779a43a4b2172884aeeba2
Jinhyun Hwang <jinhyunh0808@gmail.com>
2024-09-24 20:01:52 -0400

    Rough skeleton of landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,54 @@
 import Image from "next/image";
-import Link from 'next/link';
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+      <main className="flex flex-col gap-8 row-start-2 justify-center items-center">
         <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
+          className="dark:invert items-center"
+          src="/ico_shuffle_722.png"
           alt="Next.js logo"
-          width={180}
-          height={38}
+          width={256}
+          height={256}
           priority
         />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
+        <ol className="grid grid-cols-1 sm:grid-cols-2 gap-4 justify-items-center justify-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+          <li className="mb-4 text-center">
+            <a
+              className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
+              href="/Jinhyun_Hwang_Resume.pdf"
+              rel="noopener noreferrer"
+            >
+              Resume
+            </a>
           </li>
-          <li>Save and see your changes instantly.</li>
-          <li><Link href="/resume"> My Work</Link></li>
+          <li className="mb-4 text-center">
+            <a
+              className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
+              href="/Jinhyun_Hwang_Resume.pdf"
+              rel="noopener noreferrer"
+            >
+              Resume
+            </a>
+          </li>
+          <li className="mb-4 text-center">
+            <a
+              className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
+              href="/Jinhyun_Hwang_Resume.pdf"
+              rel="noopener noreferrer"
+            >
+              Resume
+            </a>
+          </li>
+          <li className="mb-4 text-center">
+            <a
+              className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
+              href="/Jinhyun_Hwang_Resume.pdf"
+              rel="noopener noreferrer"
+            >
+              Resume
+            </a>
+          </li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
@@ -51,7 +77,7 @@ export default function Home() {
           </a>
           <a
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="/cs5200_syllabus.pdf"
+            href="/Jinhyun_Hwang_Resume.pdf"
             rel="noopener noreferrer"
           >
             Resume
@@ -67,10 +93,10 @@ export default function Home() {
         >
           <Image
             aria-hidden
-            src="https://cmsassets.rgpub.io/sanity/images/dsfx7636/news/51e62f3c74356a7501d06feba42ac643133257d7-616x822.png"
+            src="/ico_shuffle_722.png"
             alt="File icon"
-            width={16}
-            height={16}
+            width={64}
+            height={64}
           />
         </a>
         <a
